081434f059ecf56668c3cc9f966df2fedd1f2496
Tomas Truyols <tomast25@hotmail.com>
2023-02-16 17:46:44 +0000

    style: fixed the layout of the card info page, adding a margin to the brewers tip

--- src/containers/CardInfo/CardInfo.jsx
+++ src/containers/CardInfo/CardInfo.jsx
@@ -116,68 +116,74 @@ const CardInfo = (props) => {
   });
 
   return (
-    <div className="card-info">
-      <figure className="card-info__image-container">
-        <Link to="/" className="card-info__nav-arrow">
+    <div className="card-info-page">
+      <div className="card-info">
+        <figure className="card-info__image-container">
+          <Link to="/" className="card-info__nav-arrow">
+            <img
+              src={arrow}
+              alt="arrow to go back to main menu"
+              className="card-info__nav-arrow--image"
+            />
+          </Link>
           <img
-            src={arrow}
-            alt="arrow to go back to main menu"
-            className="card-info__nav-arrow--image"
+            src={getBeer.image_url}
+            alt={`A detailed bottle of ${getBeer.name}  `}
+            className="card-info__image"
           />
-        </Link>
-        <img
-          src={getBeer.image_url}
-          alt={`A detailed bottle of ${getBeer.name}  `}
-          className="card-info__image"
-        />
-      </figure>
-      <div className="card-info__header">
-        <h2 className="card-info__title">{getBeer.name}</h2>
-        <p className="card-info__tagline">{getBeer.tagline}</p>
-      </div>
-      <div className="card-info__description">{getBeer.description}</div>
-      <div className="card-info__stats-container">
-        <div className="card-info__stat">
-          <div className="card-info__stat--label">Alcohol by Volume</div>
-          <div className="card-info__stat--value">{getBeer.abv}%</div>
+        </figure>
+        <div className="card-info__header">
+          <h2 className="card-info__title">{getBeer.name}</h2>
+          <p className="card-info__tagline">{getBeer.tagline}</p>
         </div>
-        <div className="card-info__stat">
-          <div className="card-info__stat--label">First Brewed</div>
-          <div className="card-info__stat--value">{getBeer.first_brewed}</div>
-        </div>
-        <div className="card-info__stat">
-          <div className="card-info__stat--label">Acidity</div>
-          <div className="card-info__stat--value">{getBeer.ph}</div>
-        </div>
-      </div>
-      <details className="card-info__ingredients">
-        <summary className="card-info__ingredients--title">Ingredients</summary>
-        <div className="card-info__ingredient">
-          <h3 className="card-info__ingredient--header">Hops</h3>
-          <h4 className="card-info__ingredient--added-time">Added at the Start</h4>
-          <div className="card-info__ingredient--data">{startArrJSX}</div>
-          <h4 className="card-info__ingredient--added-time">Added in the Middle</h4>
-          <div className="card-info__ingredient--data">{middleArrJSX}</div>
-          <h4 className="card-info__ingredient--added-time">Added at the End</h4>
-          <div className="card-info__ingredient--data">{endArrJSX}</div>
-          <h3 className="card-info__ingredient--header">Malts</h3>
-          <div className="card-info__ingredient--data">{getMaltJSX}</div>
-          <h3 className="card-info__ingredient--header">Yeast</h3>
-          <div className="card-info__ingredient--data">
-            {getBeer.ingredients.yeast}
+        <div className="card-info__description">{getBeer.description}</div>
+        <div className="card-info__stats-container">
+          <div className="card-info__stat">
+            <div className="card-info__stat--label">Alcohol by Volume</div>
+            <div className="card-info__stat--value">{getBeer.abv}%</div>
+          </div>
+          <div className="card-info__stat">
+            <div className="card-info__stat--label">First Brewed</div>
+            <div className="card-info__stat--value">{getBeer.first_brewed}</div>
+          </div>
+          <div className="card-info__stat">
+            <div className="card-info__stat--label">Acidity</div>
+            <div className="card-info__stat--value">{getBeer.ph}</div>
           </div>
         </div>
-      </details>
-      <details className="card-info__food-pairings">
-        <summary className="card-info__food-pairings--header">Food Pairings</summary>
-        <p className="card-info__food-pairings--text">
-          <em>
-            If you are looking for some nice foods that would match this{" "}
-            <strong>beer</strong>, we recommend a few meals like:
-          </em>
-        </p>
-        <ul>{getFoodPairingJSX}</ul>
-      </details>
+        <details className="card-info__ingredients">
+          <summary className="card-info__ingredients--title">Ingredients</summary>
+          <div className="card-info__ingredient">
+            <h3 className="card-info__ingredient--header">Hops</h3>
+            <h4 className="card-info__ingredient--added-time">Added at the Start</h4>
+            <div className="card-info__ingredient--data">{startArrJSX}</div>
+            <h4 className="card-info__ingredient--added-time">
+              Added in the Middle
+            </h4>
+            <div className="card-info__ingredient--data">{middleArrJSX}</div>
+            <h4 className="card-info__ingredient--added-time">Added at the End</h4>
+            <div className="card-info__ingredient--data">{endArrJSX}</div>
+            <h3 className="card-info__ingredient--header">Malts</h3>
+            <div className="card-info__ingredient--data">{getMaltJSX}</div>
+            <h3 className="card-info__ingredient--header">Yeast</h3>
+            <div className="card-info__ingredient--data">
+              {getBeer.ingredients.yeast}
+            </div>
+          </div>
+        </details>
+        <details className="card-info__food-pairings">
+          <summary className="card-info__food-pairings--header">
+            Food Pairings
+          </summary>
+          <p className="card-info__food-pairings--text">
+            <em>
+              If you are looking for some nice foods that would match this{" "}
+              <strong>beer</strong>, we recommend a few meals like:
+            </em>
+          </p>
+          <ul>{getFoodPairingJSX}</ul>
+        </details>
+      </div>
       <div className="card-info__brewers-tips">
         <h2 className="card-info__brewers-tips--header">Brewers Tips</h2>
         <p className="card-info__brewers-tips--text">{getBeer.brewers_tips}</p>
