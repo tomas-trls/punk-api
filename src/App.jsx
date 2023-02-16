4a4a0a3678fd0dd17a1500bebb83d3f1228e83fd
Tomas Truyols <tomast25@hotmail.com>
2023-02-16 12:31:09 +0000

    build: fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
 
 import CardInfo from "./containers/CardInfo/CardInfo";
